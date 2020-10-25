5e20debab9b60edc506a3086cbbe8aaea6895cd1
samuelmarina <samuellmarina@gmail.com>
2020-10-25 19:47:56 -0400

    Crear estructura inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { IconComponent } from './components/ui/icon/icon.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IconComponent,
+
   ],
   imports: [
     BrowserModule
