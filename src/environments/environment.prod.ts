7be47c977809bde0c5d077dc33258a42e1789dbe
samuelmarina <samuellmarina@gmail.com>
2020-10-28 00:23:32 -0400

    Install Firebase an Angular Fire

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCarOT80s8hLVmCkTe-R-gt0IKR4nqAl-w",
+    authDomain: "chucherias-cc0ae.firebaseapp.com",
+    databaseURL: "https://chucherias-cc0ae.firebaseio.com",
+    projectId: "chucherias-cc0ae",
+    storageBucket: "chucherias-cc0ae.appspot.com",
+    messagingSenderId: "185088934416",
+    appId: "1:185088934416:web:656ea3981bd3aa270f0095",
+    measurementId: "G-TBC5RJRX8C"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCarOT80s8hLVmCkTe-R-gt0IKR4nqAl-w",
+    authDomain: "chucherias-cc0ae.firebaseapp.com",
+    databaseURL: "https://chucherias-cc0ae.firebaseio.com",
+    projectId: "chucherias-cc0ae",
+    storageBucket: "chucherias-cc0ae.appspot.com",
+    messagingSenderId: "185088934416",
+    appId: "1:185088934416:web:656ea3981bd3aa270f0095",
+    measurementId: "G-TBC5RJRX8C"
+  }
 };
 
 /*
