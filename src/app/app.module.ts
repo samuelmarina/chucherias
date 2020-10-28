a52a9b41c1371e611aeba56b71ad5e69379aa02a
samuelmarina <samuellmarina@gmail.com>
2020-10-28 00:24:25 -0400

    Import Angular Fire modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from "angularfire2"
+import { AngularFireDatabaseModule} from "angularfire2/database"
+import { AngularFireAuthModule } from "angularfire2/auth"
 
 import { AppComponent } from './app.component';
 import { IconComponent } from './components/ui/icon/icon.component';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
@@ -11,7 +15,10 @@ import { IconComponent } from './components/ui/icon/icon.component';
 
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
