a0e9d429ef34b511337f376714cd80c2b5f4408d
samuelmarina <samuellmarina@gmail.com>
2020-10-29 16:21:20 -0400

    Create service to retrieve categories from the db

---
+++ src/app/services/category/category.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoryService {
+
+  constructor(private db: AngularFireDatabase) { }
+
+  getCategories() {
+    return this.db.list('/categories', ref => ref.orderByChild("name"));
+  }
+}
