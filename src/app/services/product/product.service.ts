ec37111f7590f423b19214f24a913a4f7d072bd2
samuelmarina <samuellmarina@gmail.com>
2020-10-29 22:35:29 -0400

    Modify Angular Fire import

--- src/app/services/product/product.service.ts
+++ src/app/services/product/product.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireDatabase } from "@angular/fire/database"
 
 @Injectable({
   providedIn: 'root'
@@ -10,5 +10,13 @@ export class ProductService {
 
   create(product){
     return this.db.list("/products").push(product);
+  }
+
+  getAll() {
+    return this.db.list("/products").valueChanges();
+  }
+
+  getProduct(productId){
+    return this.db.object("/products/" + productId);
   }
 }
