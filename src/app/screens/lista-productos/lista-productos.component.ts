5c16a2bbfbaa29ab4a6587010267c251f0929a01
samuelmarina <samuellmarina@gmail.com>
2020-10-31 00:06:17 -0400

    Create initial product card

--- src/app/screens/lista-productos/lista-productos.component.ts
+++ src/app/screens/lista-productos/lista-productos.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { ProductService } from 'src/app/services/product/product.service';
 
 @Component({
   selector: 'app-lista-productos',
@@ -6,8 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lista-productos.component.css']
 })
 export class ListaProductosComponent implements OnInit {
-
-  constructor() { }
+  products$;
+  
+  constructor(private productService: ProductService) {
+    this.products$ = this.productService.getAll().snapshotChanges().pipe(
+      map(changes => changes.map(c => (
+        {
+          key: c.key, 
+          title: c.payload.val()['title'],
+          price: c.payload.val()['price'],
+          imageUrl: c.payload.val()['imageUrl']
+        })))
+    )
+   }
 
   ngOnInit(): void {
   }
