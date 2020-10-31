16d51fbedb6d0b7065e2a6474a2f7f5c21c97af7
samuelmarina <samuellmarina@gmail.com>
2020-10-30 23:43:54 -0400

    Create table with pagination

--- src/app/screens/admin/admin-productos/admin-productos.component.ts
+++ src/app/screens/admin/admin-productos/admin-productos.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { map } from 'rxjs/operators';
 import { ProductService } from 'src/app/services/product/product.service';
+import {MatTableDataSource} from '@angular/material/table';
+import { async } from '@angular/core/testing';
 
 @Component({
   selector: 'app-admin-productos',
@@ -9,14 +11,23 @@ import { ProductService } from 'src/app/services/product/product.service';
 })
 export class AdminProductosComponent implements OnInit {
   products$;
+  displayedColumns: string[] = ["title", "price", "edit"]
 
   constructor(private productService: ProductService) { 
     this.products$ = this.productService.getAll().snapshotChanges().pipe(
-      map(changes => changes.map(c => ({key: c.key, val: c.payload.val()})))
+      map(changes => changes.map(c => (
+        {
+          key: c.key, 
+          title: c.payload.val()['title'],
+          price: c.payload.val()['price']
+        })))
     )
+    
   }
 
+
   ngOnInit(): void {
+
   }
 
 }
