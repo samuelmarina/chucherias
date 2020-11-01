c400924cecc96f6052d188dd509c60735336e400
samuelmarina <samuellmarina@gmail.com>
2020-11-01 14:24:39 -0400

    Add button and pagination

--- src/app/screens/admin/admin-productos/admin-productos.component.ts
+++ src/app/screens/admin/admin-productos/admin-productos.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { map } from 'rxjs/operators';
 import { ProductService } from 'src/app/services/product/product.service';
 import {MatTableDataSource} from '@angular/material/table';
-import { async } from '@angular/core/testing';
+import {MatPaginator} from '@angular/material/paginator';
 
 @Component({
   selector: 'app-admin-productos',
@@ -11,17 +11,29 @@ import { async } from '@angular/core/testing';
 })
 export class AdminProductosComponent implements OnInit {
   products$;
+  dataSource: MatTableDataSource<any>;
+  elementData = [];
   displayedColumns: string[] = ["title", "price", "edit"]
+  @ViewChild(MatPaginator) paginator: MatPaginator;
 
   constructor(private productService: ProductService) { 
-    this.products$ = this.productService.getAll().snapshotChanges().pipe(
-      map(changes => changes.map(c => (
-        {
-          key: c.key, 
-          title: c.payload.val()['title'],
-          price: c.payload.val()['price']
-        })))
+    // this.products$ = this.productService.getAll().snapshotChanges().pipe(
+    //   map(changes => changes.map(c => (
+    //     {
+    //       key: c.key, 
+    //       title: c.payload.val()['title'],
+    //       price: c.payload.val()['price']
+    //     })))
+    // )
+
+    this.productService.getAll().valueChanges().pipe(
+      map(changes => changes.map(c => c))
     )
+    .subscribe(c => {
+      c.map(k => this.elementData.push(k))
+      this.dataSource = new MatTableDataSource(this.elementData);
+      this.dataSource.paginator = this.paginator;
+    });
     
   }
 
