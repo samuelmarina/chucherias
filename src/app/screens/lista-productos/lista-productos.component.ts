2f2c019daaabd08e70ec6de22e91b6843f0af72a
samuelmarina <samuellmarina@gmail.com>
2020-10-31 23:12:45 -0400

    Filter products by category using the list menu

--- src/app/screens/lista-productos/lista-productos.component.ts
+++ src/app/screens/lista-productos/lista-productos.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { map } from 'rxjs/operators';
+import { ActivatedRoute } from '@angular/router';
+import { map, switchMap } from 'rxjs/operators';
 import { CategoryService } from 'src/app/services/category/category.service';
 import { ProductService } from 'src/app/services/product/product.service';
 
@@ -9,28 +10,56 @@ import { ProductService } from 'src/app/services/product/product.service';
   styleUrls: ['./lista-productos.component.css']
 })
 export class ListaProductosComponent implements OnInit {
-  products$;
+  products: any[] = [];
+  filteredProducts = [];
   categories$;
+  category: string;
   
-  constructor(private productService: ProductService, private categoryService: CategoryService) {
-    this.products$ = this.productService.getAll().snapshotChanges().pipe(
-      map(changes => changes.map(c => (
-        {
-          key: c.key, 
-          title: c.payload.val()['title'],
-          price: c.payload.val()['price'],
-          imageUrl: c.payload.val()['imageUrl']
-        })))
+  constructor(
+    route: ActivatedRoute,
+    private productService: ProductService, 
+    private categoryService: CategoryService) {
+
+    // this.products$ = this.productService.getAll().snapshotChanges().pipe(
+    //   map(changes => changes.map(c => (
+    //     {
+    //       key: c.key, 
+    //       title: c.payload.val()['title'],
+    //       price: c.payload.val()['price'],
+    //       imageUrl: c.payload.val()['imageUrl']
+    //     })))
+    // )
+
+    this.productService.getAll().valueChanges().pipe(
+      map(changes => changes.map(c => c))
     )
+    .subscribe(c => {
+      c.map(k => this.products.push(k))
+      route.queryParamMap.subscribe(params => {
+        this.category = params.get('category');
+
+        this.filteredProducts = this.category ?
+        this.products.filter(p => p['category'] === this.category) :
+        this.products;
+
+      })
+    });
+
 
     this.categories$ = categoryService.getCategories().snapshotChanges().pipe(
       map(changes => changes.map(c => ({key: c.payload.key, name: c.payload.val()})))
     )
-
-    console.log(this.categories$)
+    
    }
 
   ngOnInit(): void {
   }
 
+}
+
+export interface Product {
+  title: string,
+  price: number,
+  category: string,
+  imageUrl: string
 }
