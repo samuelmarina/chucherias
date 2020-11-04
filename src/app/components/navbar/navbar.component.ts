f7187670024127650755ada7a4315f914d9063e6
unknown <rommel.sanzonetti@correo.unimet.edu.ve>
2020-11-04 19:52:19 -0400

    'navbarlisto1'

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -6,7 +6,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  logged:boolean=true;
+  
+  @Input() logged: boolean = true;
+  
   constructor() { }
 
   ngOnInit(): void {
