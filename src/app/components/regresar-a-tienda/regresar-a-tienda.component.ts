5a114fc23efbddfd3f7072be4fa2f66f67884df3
unknown <rommel.sanzonetti@correo.unimet.edu.ve>
2020-11-08 18:58:34 -0400

    regresar a la tienda component

---
+++ src/app/components/regresar-a-tienda/regresar-a-tienda.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-regresar-a-tienda',
+  templateUrl: './regresar-a-tienda.component.html',
+  styleUrls: ['./regresar-a-tienda.component.css']
+})
+export class RegresarATiendaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
