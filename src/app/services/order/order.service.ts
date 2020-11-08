12fa420a0e7c59fc0c419835d2a23834ed21f990
samuelmarina <samuellmarina@gmail.com>
2020-11-08 12:51:28 -0400

    Create service for orders

---
+++ src/app/services/order/order.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/database';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrderService {
+
+  constructor(private db: AngularFireDatabase) { }
+
+  getall() {
+    return this.db.list("/orders");
+  }
+}
