e4c60d048ffea904cf8effe71c2ce80f0af73df9
samuelmarina <samuellmarina@gmail.com>
2020-11-18 00:46:55 -0400

    Create function to add products to bags

--- src/app/services/shopping-bag/shopping-bag.service.ts
+++ src/app/services/shopping-bag/shopping-bag.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from '@angular/fire/database';
+import { take } from 'rxjs/operators';
+import { Producto } from 'src/app/schemas/producto';
+import firebase from "firebase/app"
 
 @Injectable({
   providedIn: 'root'
@@ -8,9 +11,61 @@ export class ShoppingBagService {
 
   constructor(private db: AngularFireDatabase) { }
 
-  create() {
+  private create() {
     return this.db.list("/shopping-bags").push({
       dateCreated: new Date().getTime()
     })
+  }
+
+  private getBag(bagId: string){
+    return this.db.object("shopping-bags/" + bagId);
+  }
+
+  private async getOrCreateBagId(){
+    let bagId = localStorage.getItem('bagId');
+    if(bagId) return bagId;
+    
+    let res = await this.create();
+      localStorage.setItem("bagId", res.key);
+      return res.key;
+  }
+
+  async addToBag(product: Producto) {
+    let bagId = await this.getOrCreateBagId();
+    let price = product.price.toString().replace(".", ",");
+    let item$ = this.db.object("/shopping-bags/" + bagId + "/items/" + price);
+    let product$ = this.db.object("/shopping-bags/" + bagId + "/items/" + price + "/products/" + product.key);
+    let ref = firebase.database().ref("/shopping-bags/" + bagId + "/items/" + price + "/products/" + product.key);
+    let isProductAdded = await this.isProductAdded(ref);
+
+    item$.valueChanges().pipe(take(1)).subscribe(item => {
+      if(!item) {
+        item$.set({
+          quantity: 50
+        })
+        product$.set({
+          product: product
+        })
+        
+      }
+      else{
+        item$.update({
+          quantity: item['quantity'] + 50
+        })
+        if(!isProductAdded){
+          product$.set({
+            product: product
+          })
+        }
+      }
+    });
+  }
+
+  private async isProductAdded(ref: firebase.database.Reference) {
+    let flag;
+    await ref.once("value").then(res => {
+      flag = res.exists();
+    })
+    return flag;
   }
 }
