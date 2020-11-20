110702e53fa420ed92408f57ee3286849ec1cc61
samuelmarina <samuellmarina@gmail.com>
2020-11-20 16:32:55 -0400

    Add aditional functions to category service

--- src/app/services/category/category.service.ts
+++ src/app/services/category/category.service.ts
@@ -11,4 +11,21 @@ export class CategoryService {
   getCategories() {
     return this.db.list('/categories', ref => ref.orderByChild("name"));
   }
+
+  create(category: string) {
+    let key = category.toLowerCase();
+    return this.db.list('/categories').set(key, {name: category});
+  }
+
+  getCategory(categoryId){
+    return this.db.object("/categories/" + categoryId);
+  }
+
+  update(categoryId, category){
+    return this.db.object("/categories/" + categoryId).update(category)
+  }
+
+  delete(categoryId){
+    return this.db.object("/categories/" + categoryId).remove();
+  }
 }
