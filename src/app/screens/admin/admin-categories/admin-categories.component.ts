7c83933a5f048a34e16526e6e2dc930d29019cb9
samuelmarina <samuellmarina@gmail.com>
2020-11-20 16:47:40 -0400

    Include table on categories screen

--- src/app/screens/admin/admin-categories/admin-categories.component.ts
+++ src/app/screens/admin/admin-categories/admin-categories.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { map } from 'rxjs/operators';
+import { CategoryService } from 'src/app/services/category/category.service';
 
 @Component({
   selector: 'app-admin-categories',
@@ -6,8 +10,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-categories.component.css']
 })
 export class AdminCategoriesComponent implements OnInit {
+  elementData = [];
+  dataSource: MatTableDataSource<any>;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  displayedColumns: string[] = ["name", "edit"];
 
-  constructor() { }
+  constructor(
+    private categoryService: CategoryService
+  ) {
+    this.categoryService.getCategories().snapshotChanges().pipe(
+      map(changes => changes.map(c => c))
+    )
+    .subscribe(c => {
+      c.map(k => {
+        this.elementData.push({
+          key: k.key,
+          ...k.payload.val() as any
+        })
+      })
+      this.dataSource = new MatTableDataSource(this.elementData);
+      this.dataSource.paginator = this.paginator;
+    })
+
+   }
 
   ngOnInit(): void {
   }
