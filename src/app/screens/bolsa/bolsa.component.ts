c2e4dfe964cc84071b5f87f7524b6afb0d26016f
Gebrayel <gebrayelinatti06@gmail.com>
2020-11-20 15:54:00 -0400

    conectar emptypage a base de datos

--- src/app/screens/bolsa/bolsa.component.ts
+++ src/app/screens/bolsa/bolsa.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'firebase';
+import { AuthService } from 'src/app/services/auth/auth.service';
+import { ShoppingBagService } from 'src/app/services/shopping-bag/shopping-bag.service';
+import { UserService } from 'src/app/services/user/user.service';
+import firebase from "firebase/app";
 
 @Component({
   selector: 'app-bolsa',
@@ -6,7 +11,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bolsa.component.css']
 })
 export class BolsaComponent implements OnInit {
-  constructor() { }
+  user: User;
+  booleano: boolean;
 
-  ngOnInit() {} 
+  constructor(private auth: AuthService) { }
+
+  async ngOnInit() {
+    this.auth.user$.subscribe(user => {
+      this.user = user;
+      let ref = firebase.database().ref("/users/" + user.uid + "/shopping-bags/");
+      ref.once("value").then(res => {
+        this.booleano = res.exists();
+        console.log(this.booleano);
+        
+      })
+  })
 }
+} 
+
