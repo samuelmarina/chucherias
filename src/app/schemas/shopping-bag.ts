e0fd3858074df03abc73b3187dd52b264d2ca22e
samuelmarina <samuellmarina@gmail.com>
2020-11-19 23:56:33 -0400

    Create schemas for shopping bags

--- src/app/schemas/shopping-bag.ts
+++ src/app/schemas/shopping-bag.ts
@@ -14,3 +14,13 @@ export interface ShoppingBag {
     items: Prices[];
     quantity: number;
 }
+
+export interface uiShoppingBag {
+    price: string;
+    quantity: number;
+    bags: uiIndividualBag[];
+}
+
+export interface uiIndividualBag {
+    product: Producto[];
+}
