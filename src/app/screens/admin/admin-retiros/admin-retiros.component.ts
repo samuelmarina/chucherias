c5a006380ce46adb7c5bc2be09f4f7058ac10932
samuelmarina <samuellmarina@gmail.com>
2020-11-23 22:07:58 -0400

    Add table and button for retiros screen component

--- src/app/screens/admin/admin-retiros/admin-retiros.component.ts
+++ src/app/screens/admin/admin-retiros/admin-retiros.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { map } from 'rxjs/operators';
+import { RetiroService } from 'src/app/services/retiro/retiro.service';
 
 @Component({
   selector: 'app-admin-retiros',
@@ -6,8 +10,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-retiros.component.css']
 })
 export class AdminRetirosComponent implements OnInit {
+  dataSource: MatTableDataSource<any>;
+  elementData = [];
+  displayedColumns: string[] = ["name", "edit"];
+  @ViewChild(MatPaginator) paginator: MatPaginator;
 
-  constructor() { }
+  constructor(
+    private retiroService: RetiroService
+  ) {
+    this.retiroService.getAll().snapshotChanges().pipe(
+      map(changes => changes.map(c => c))
+    )
+    .subscribe(c => {
+      c.map(k => {
+        this.elementData.push({
+          key: k.key,
+          ...k.payload.val() as any
+        })
+      })
+      this.dataSource = new MatTableDataSource(this.elementData);
+      this.dataSource.paginator = this.paginator;
+    })
+
+   }
 
   ngOnInit(): void {
   }
