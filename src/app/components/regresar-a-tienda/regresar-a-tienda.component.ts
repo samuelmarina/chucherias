183250b1170c6b9af3fb022c66f66171255f5806
Gebrayel <gebrayelinatti06@gmail.com>
2020-11-26 05:00:32 -0400

    Agregar pagina actual a componente regresar

--- src/app/components/regresar-a-tienda/regresar-a-tienda.component.ts
+++ src/app/components/regresar-a-tienda/regresar-a-tienda.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Url } from 'url';
 
 @Component({
   selector: 'app-regresar-a-tienda',
@@ -6,10 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./regresar-a-tienda.component.css']
 })
 export class RegresarATiendaComponent implements OnInit {
-
+  urlActual;
+  prevUrl;
+  booleano:boolean = false;
+  page: string;
   constructor() { }
 
   ngOnInit(): void {
+    this.urlActual = window.location;
+    if (this.urlActual.pathname == '/bolsa') {
+      this.page = "Chucheribolsa";
+    } else if (this.urlActual.pathname == '/carrito'){
+        this.page = "Chuchericarro";
+    } else if (this.urlActual.pathname == '/wishlist'){
+        this.page = "Chucherideseos"
+    } else if (this.urlActual.pathname == '/producto'){
+      this.page = "Chucheriproductos"
+    }else if (this.urlActual.pathname == '/check-out'){
+      this.page = "Chuchericheckout"
+    }else if (this.urlActual.pathname == '/tracking'){
+      this.page = "Chucheritracking"
+    }else {
+      this.page = "Chucheriproductos"
+    }
+
+    
   }
 
 }
