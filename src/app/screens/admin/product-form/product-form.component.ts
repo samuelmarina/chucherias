cb94d49b3e6e0c17a71459a4f4547b0bb2f6d70f
Gebrayel <gebrayelinatti06@gmail.com>
2020-12-10 05:14:56 -0400

    Codigo comentado en product form

--- src/app/screens/admin/product-form/product-form.component.ts
+++ src/app/screens/admin/product-form/product-form.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+// import { AngularFireStorage } from '@angular/fire/storage';
 import { ActivatedRoute, Router } from '@angular/router';
-import { map, take } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { finalize, map, take } from 'rxjs/operators';
 import { Producto } from 'src/app/schemas/producto';
 import { CategoryService } from 'src/app/services/category/category.service';
 import { ProductService } from 'src/app/services/product/product.service';
+
 
 @Component({
   selector: 'app-product-form',
@@ -11,6 +14,10 @@ import { ProductService } from 'src/app/services/product/product.service';
   styleUrls: ['./product-form.component.css']
 })
 export class ProductFormComponent {
+  private filePath:any;
+  private image:any;
+  private downloadUrl: string;
+  private downloadUrl2: string = "hola";
   categories$;
   product: Producto = {
     key: null,
@@ -27,7 +34,9 @@ export class ProductFormComponent {
     private router: Router,
     private categoryService: CategoryService, 
     private route: ActivatedRoute,
-    private productService: ProductService) { 
+    private productService: ProductService,
+    // private storage: AngularFireStorage
+    ) { 
     this.categories$ = categoryService.getCategories().snapshotChanges().pipe(
       map(changes => changes.map(c => ({key: c.payload.key, name: c.payload.val()})))
     )
@@ -72,4 +81,25 @@ export class ProductFormComponent {
       this.router.navigate(['admin/productos']);
     }
   }
+
+  // handleImage(event:any):void{
+  //   this.image = event.target.files[0];
+  //   console.log('IMG: ', this.image);
+  //   this.filePath = `images/${this.image.name}`;
+  //   const fileRef = this.storage.ref(this.filePath);
+  //   const task = this.storage.upload(this.filePath, this.image);
+    
+  //   task.snapshotChanges().pipe(
+  //     finalize(()=>{
+  //       fileRef.getDownloadURL().subscribe( urlImage =>{
+  //         this.downloadUrl = urlImage;
+  //         console.log('URL IMAGE: ', this.downloadUrl);
+  //         document.getElementById("imageUrl").innerHTML = this.downloadUrl
+  //       })
+  //     })
+  //   ).subscribe();
+    
+      
+  // }
+
 }
